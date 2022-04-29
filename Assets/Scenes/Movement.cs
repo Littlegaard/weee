993e015e7eabc2db7f9459e485f3a56ad38709be
Littlegaard <68397188+Littlegaard@users.noreply.github.com>
2022-04-29 21:04:11 +0200

    Initial commit

---
+++ Assets/Scenes/Movement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+
+{
+    //movement
+  public float Movementspeed = 10;
+ 
+
+  //jump
+    
+ 
+
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       var movement = UnityEngine.Input.GetAxis("Horizontal");
+       transform.position += new Vector3(movement,0,0) * Time.deltaTime * Movementspeed;
+
+    }
+} 
+
