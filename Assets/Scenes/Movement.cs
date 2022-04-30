fe0cc250e8104d3ed258d7e7d49cdc684848bb07
Littlegaard <68397188+Littlegaard@users.noreply.github.com>
2022-04-30 21:52:38 +0200

    wee
    sad

--- Assets/Scenes/Movement.cs
+++ Assets/Scenes/Movement.cs
@@ -5,25 +5,42 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 
 {
-    //movement
-  public float Movementspeed = 10;
+    //animation
+    private Animator _aniL;
 
-  //jump
-    //sejt
-    //sejt
+
+    //movement
+    public float Movementspeed;
+ 
+    //jump
+    private Rigidbody2D _RB;
+    public float Jumpforce;
  
 
     void Start()
     {
+        //jump
+    _RB = GetComponent<Rigidbody2D>();
+    _aniL = GetComponent<Animator>();
     
+    //animation
+  
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        //movement
        var movement = UnityEngine.Input.GetAxis("Horizontal");
        transform.position += new Vector3(movement,0,0) * Time.deltaTime * Movementspeed;
+       //jump
 
+       if(UnityEngine.Input.GetButtonDown("Jump") && Mathf.Abs(_RB.velocity.y) < 0.001f)
+       {
+           _RB.AddForce(new Vector2(0, Jumpforce), ForceMode2D.Impulse);
+       }
+       //animation
+       
     }
 } 
-
